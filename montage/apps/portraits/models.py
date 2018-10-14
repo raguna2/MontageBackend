00cc09b3320072058a1207b7e97d36c6d8403a4d
Kutsumi <shintaro.kutsumi@beproud.jp>
2018-10-14 18:57:39 +0900

    モデルのHearsayをImporessionへ変更

--- montage/apps/portraits/models.py
+++ montage/apps/portraits/models.py
@@ -60,50 +60,50 @@ class Question(models.Model):
         return f'Q: {self.about}'
 
 
-class HearsayQuerySet(models.query.QuerySet):
+class ImpressionQuerySet(models.query.QuerySet):
     def not_collaged(self):
-        """collageされていないものを抽出"""
+        """collageされていないImpressionを抽出"""
         return self.filter(is_collaged=False)
 
 
-class Hearsay(models.Model):
-    objects = HearsayQuerySet().as_manager()
+class Impression(models.Model):
+    objects = ImpressionQuerySet().as_manager()
 
     class Meta:
-        verbose_name = 'Hearsay'
-        verbose_name_plural = 'Hearsays'
+        verbose_name = 'Impression'
+        verbose_name_plural = 'Impressions'
         ordering = ('-posted_at', )
 
-    # 逆参照時: Impression.rev_hearsay.all()
-    # Impressionがなくなったとき、画面には表示しなくなるがデータは残す
-    impression = models.ForeignKey(
-        Impression,
-        related_name='rev_hearsay',
+    # 逆参照時: Question.rev_impression.all()
+    # Questionがなくなったとき、画面には表示しなくなるがデータは残す
+    question = models.ForeignKey(
+        Question,
+        related_name='rev_impression',
         blank=True,
         null=True,
         default=None,
         on_delete=models.SET_NULL)
     user = models.ForeignKey(
         MontageUser,
-        related_name='rev_hearsay',
+        related_name='rev_impression',
         blank=True,
         null=True,
         default=None,
         on_delete=models.SET_NULL)
     content = models.CharField(
-        'うわさ',
+        '内容',
         max_length=42,
-        help_text='うわさの内容',
+        help_text='質問に対してその人に対して持っている印象の内容',
     )
     posted_at = models.DateTimeField(
-        '作成日', help_text='うわさが投稿された日', auto_now_add=True)
+        '投稿日', help_text='投稿された日', auto_now_add=True)
     is_collaged = models.BooleanField(
         '変更されているか',
         help_text='collageされた場合画面には表示させないためにTrueにする',
         default=False)
 
     def __str__(self):
-        """Printしたときはaboutを返す"""
+        """Printしたときはcontentを返す"""
         return self.content
 
     @property
