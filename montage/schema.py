d308ee8ba24a4b83299110234eee8b2d57069410
Kutsumi <shintaro.kutsumi@beproud.jp>
2018-11-02 23:27:57 +0900

    query,mutationのインポート整理

--- montage/schema.py
+++ montage/schema.py
@@ -1,29 +1,22 @@
 import graphene
 
-# MontageUser
-from montage.schemas.user_schema import (
-    UserType, Mutation as user_mute, Query as user_query)
-
-# Category
-from montage.schemas.category_schema import (
-    CategoryType, Mutation as category_mute, Query as category_query)
-
-# Impression
-from montage.schemas.impression_schema import (
-    ImpressionType, Query as impression_query, Mutation as impression_mute)
-
-# Question
-from montage.schemas.question_schema import (
-    QuestionType, Query as question_query, Mutation as question_mute)
+from montage.schemas import (
+    user_mute, user_query,
+    impression_mute, impression_query,
+    question_mute, question_query,
+    category_mute, category_query,
+    relate_mute, relate_query,
+    # friendship_mute, friendship_query,
+)
 
 
 class Mutation(user_mute, category_mute, impression_mute,
-               question_mute, graphene.ObjectType):
+               question_mute, relate_mute, graphene.ObjectType):
     pass
 
 
 class Query(user_query, category_query, impression_query,
-            question_query, graphene.ObjectType):
+            question_query, relate_query, graphene.ObjectType):
     pass
 
 
