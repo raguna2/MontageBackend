00b55c3bfa37c240c780d36c926945c1d68bf18f
raguna2 <kutsumi.for.public@gmail.com>
2020-04-11 16:08:26 +0900

    テンプレートを削除し、デプロイ用Dockerfileも修正

--- montage/apps/accounts/views.py
+++ montage/apps/accounts/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render
-
-from .models import MontageUser
+from django.http import HttpResponse
 
 
 def mainview(request):
-    user = MontageUser.objects.get(username='montage')
-    as_atsign = user.as_atsign
-    context = {'user': user, 'as_atsign': as_atsign}
-    return render(request, 'user.html', context)
+    return HttpResponse('Hello, World!')
